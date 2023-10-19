61754880b7b4fc7360327e656eb5eae6d22f685e
Earnest Chestnut Jr <earnest@pop-os.localdomain>
2023-10-19 07:37:14 -0400

    Add product queries to get and search all products

--- lib/product_search_app_web/schema.ex
+++ lib/product_search_app_web/schema.ex
@@ -12,4 +12,20 @@ defmodule ProductSearchAppWeb.Schema do
     field :about, non_null(:string)
     field :tags, non_null(:string)
   end
+
+  query do
+    # this is the query entry point to our app
+    @desc "Get all products"
+    field :all_products, non_null(list_of(non_null(:product))) do
+      resolve(&CatalogResolver.all_products/3)
+    end
+
+    @desc "Search all products"
+    field :search_products, non_null(list_of(non_null(:product))) do
+      arg(:search_query, :string)
+      arg(:search_result_limit, :integer)
+
+      resolve(&CatalogResolver.search_products/3)
+    end
+  end
 end
